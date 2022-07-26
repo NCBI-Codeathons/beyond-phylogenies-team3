6ee6e6923e15629d359905087feabcd75397166e
cmaceves <christineaceves22@gmail.com>
2022-07-26 12:40:11 -0700

    outlining steps to render

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import parse_vcf
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def home():   
-   render_template("./src/display.html")
+def home(): 
+    #load tree
+    #output tree as string
+    #load vcf and output psuedo alignment
+    #render the html to webpage
+    render_template("./src/display.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
