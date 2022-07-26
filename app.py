9ad2b16d48ed4c3b2d08e897815a97a14f539e41
cmaceves <christineaceves22@gmail.com>
2022-07-26 12:35:27 -0700

    changing app to pull display

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():   
-   return("Hello World")
+   render_template("./src/display.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
